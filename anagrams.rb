cd8daf04e7e86bb1f8edb3e600dc324199ab2a60
Bennett <drbmgit@gmail.com>
2022-10-07 15:10:48 -0400

    2nd anagram

--- anagrams.rb
+++ anagrams.rb
@@ -5,9 +5,12 @@ end
 
 def second_anagram?(str_1, str_2)
     str_2_arr = str_2.split("")
-    str_1.each_char.with_index do |char,idx|
+    str_1.each_char do |char|
         str_2_arr.delete(char)
     end
+    return str_2_arr == []
 
-    
 end
+# O(n)
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")
