d1c45e9a6bf2b5450303baaa0d92176e00fc22aa
Arnob <arnobdam@gmail.com>
2022-10-07 15:37:52 -0400

    completed anagrams.rb

--- anagrams.rb
+++ anagrams.rb
@@ -1,7 +1,17 @@
 def first_anagram?(str_1, str_2)
 
+    str_1_arr = str_1.split("")
 
+    permutations = str_1_arr.permutation
+
+    return true if permutations.include?(str_2.split(""))
+
+    false
 end
+#O(n!)
+
+p first_anagram?("gizmo", "sally")    #=> false
+p first_anagram?("elvis", "lives")
 
 def second_anagram?(str_1, str_2)
     str_2_arr = str_2.split("")
@@ -24,3 +34,25 @@ end
 # O(nlogn)
 # p third_anagram?("gizmo", "sally")    #=> false
 # p third_anagram?("elvis", "lives")
+
+def fourth_anagram?(str_1, str_2) #bonus version
+    hash_1 = Hash.new(0)
+    # hash_2 = Hash.new(0)
+
+    str_1.each_char do |char|
+        hash_1[char] += 1
+    end
+
+    str_2.each_char do |char|
+        hash_1[char] -= 1
+    end
+
+    return true if hash_1.values.all? {|value| value == 0}
+
+    false
+
+end
+#O(n)
+
+# p fourth_anagram?("gizmo", "sally")    #=> false
+# p fourth_anagram?("elvis", "lives")
