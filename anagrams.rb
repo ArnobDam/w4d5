91c42fbeb8fcf1fdac86bd0c3b59cc698d81ae7f
Bennett <drbmgit@gmail.com>
2022-10-07 15:15:45 -0400

    third anagram done

--- anagrams.rb
+++ anagrams.rb
@@ -14,3 +14,13 @@ end
 # O(n)
 # p second_anagram?("gizmo", "sally")    #=> false
 # p second_anagram?("elvis", "lives")
+
+def third_anagram?(str_1, str_2)
+    str_1_arr = str_1.split("")
+    str_2_arr = str_2.split("")
+
+    str_1_arr.sort == str_2_arr.sort
+end
+# O(nlogn)
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")
